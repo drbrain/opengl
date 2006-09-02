8eadf784a5f6ae55abc07856ac0ae0ad116e613b
John Gabriele <jmg3000@gmail.com>
2006-09-02 23:04:59 +0000

    Seems to work now on GNU/Linux.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ruby-opengl/trunk@47 0bf6db90-c31b-0410-8265-cbe0ef131713

--- ext/glut/extconf.rb
+++ ext/glut/extconf.rb
@@ -2,9 +2,6 @@
 # Last edit by previous maintainer:
 # 2003/08/17 03:04:32, yoshi
 #
-# $Date: 2006/07/11 05:04:08 $
-# $Author: john $
-#
 # Copyright (C) 1999 - 2005 Yoshi <yoshi@giganet.net>
 # Copyright (C) 2006 John M. Gabriele <jmg3000@gmail.com>
 #
@@ -19,9 +16,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
-# The two extension modules being built here are "opengl" and "glut".
-# (The one named "opengl" contains GLU as well.)
+# This is the extconf.rb file for creating the glut module.
 
 require "mkmf"
 
@@ -29,15 +24,7 @@ gl_libname = ""
 glu_libname = ""
 glut_libname = ""
 
-modules = ""
-ogl_flg = false
-glut_flg = false
-
-File.unlink( "Makefile" )      if ( FileTest.exist? "Makefile" )
-File.unlink( "Makefile.ogl" )  if ( FileTest.exist? "Makefile.ogl" )
-File.unlink( "Makefile.glut" ) if ( FileTest.exist? "Makefile.glut" )
-
-header = nil
+File.unlink( "Makefile" ) if ( FileTest.exist? "Makefile" )
 
 case RUBY_PLATFORM
     when /cygwin/
@@ -45,7 +32,6 @@ case RUBY_PLATFORM
         gl_libname = "opengl32"
         glu_libname = "glu32"
         glut_libname = "glut32"
-        header=[ "GL/gl.h", "GL/glu.h", "GL/glut.h" ]
 
     when /mswin32/
         $CFLAGS = "-DWIN32"
@@ -63,7 +49,6 @@ case RUBY_PLATFORM
         glut_libname = "glut"
 
     else
-        # $CPPFLAGS += " -I."  <--- Not sure why that was there since this is a C module (not C++).
         idir, ldir = dir_config( "x11", "/usr/X11R6" )
 
         have_library( "Xi",   "XAllowDeviceEvents" )
@@ -76,11 +61,15 @@ case RUBY_PLATFORM
         glut_libname = "glut"
 end
 
-dir_config( "opengl", idir, ldir )
 dir_config( "glut", idir, ldir )
 
+def have_gl_library( lib, func = 'main' )
+    have_library( lib, func ) ||
+    have_library( "Mesa" + lib, func ) ||
+    find_library( lib, func, "/System/Library/Frameworks/OpenGL.framework/Libraries/" )
+end
 
-def have_ogl_library( lib, func = 'main' )
+def have_glu_library( lib, func = 'main' )
     have_library( lib, func ) ||
     have_library( "Mesa" + lib, func ) ||
     find_library( lib, func, "/System/Library/Frameworks/OpenGL.framework/Libraries/" )
@@ -102,7 +91,6 @@ def have_glut_library( lib, func = 'main' )
     File.exist?("/System/Library/Frameworks/GLUT.framework")
 end
 
-
 # In Yoshi's original readme, he had this to say about the following
 # have_library call:
 # > Note: if a sample app crashes with an X error, comment out the line
@@ -110,61 +98,12 @@ end
 # > recompile the module, and try again.
 #have_library( "pthread", "pthread_create" )
 
-
-$objs = [ "glu.o", "ogl.o", "rbogl.o" ]
-if have_ogl_library( gl_libname ) && have_ogl_library( glu_libname )
-    create_makefile( "opengl" )
-    File.rename( "Makefile", "Makefile.ogl" )
-    modules = modules + "opengl.#{CONFIG['DLEXT']}"
-    ogl_flg = true
+# $objs = [ "glu.o", "ogl.o", "rbogl.o" ]
+if have_gl_library( gl_libname ) &&
+   have_glu_library( glu_libname) &&
+   have_glut_library( glut_libname )
+    create_makefile( "glut" )
 else
-    puts "Can't create OpenGL module!"
+    puts "Error: Couldn't create the makefile for the GLUT module."
     exit 1
 end
-
-
-$objs = [ "glut.o" ]
-if have_glut_library( glut_libname )
-    create_makefile( "glut" )
-    File.rename( "Makefile", "Makefile.glut" )
-    modules = "glut.#{CONFIG['DLEXT']} " + modules
-    glut_flg = true
-end
-
-
-# Now that the two makefiles that build the actual extension modules
-# are ready, we'll need a master makefile to delegate work to them.
-open( "Makefile", "w" ) { |f|
-    v = $nmake ? '{$(srcdir)}' : ''
-    f.write <<EOT
-SHELL = /bin/sh
-srcdir = #{$srcdir}
-VPATH = $(srcdir)
-
-all: #{modules}
-
-opengl.#{CONFIG['DLEXT']}: #{v}rbogl.c #{v}ogl.c #{v}glu.c #{v}rbogl.h
-	@echo "Now Making opengl extension module..."
-	@$(MAKE) -f Makefile.ogl
-
-glut.#{CONFIG['DLEXT']}: #{v}glut.c
-	@echo "Now Making glut extension module..."
-	@$(MAKE) -f Makefile.glut
-
-clean:
-#{"\t@$(MAKE) -f Makefile.ogl clean" if (ogl_flg)}
-#{"\t@$(MAKE) -f Makefile.glut clean" if (glut_flg)}
-
-distclean:
-#{"\t@$(MAKE) -f Makefile.ogl distclean" if (ogl_flg)}
-#{"\t@$(MAKE) -f Makefile.glut distclean" if (glut_flg)}
-
-install: #{modules}
-#{"\t@$(MAKE) -f Makefile.ogl install" if (ogl_flg)}
-#{"\t@$(MAKE) -f Makefile.glut install" if (glut_flg)}
-
-site-install: #{modules}
-#{"\t@$(MAKE) -f Makefile.ogl site-install" if (ogl_flg)}
-#{"\t@$(MAKE) -f Makefile.glut site-install" if (glut_flg)}
-EOT
-}
