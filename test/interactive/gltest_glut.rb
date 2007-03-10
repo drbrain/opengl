b8df7ab2eaee97083970e79e7204921daca5449a
Jan Dvorak <jan.dvorak@kraxnet.cx>
2007-03-10 18:32:14 +0000

    Replaced two placeholder tests in tc_base for real ones: GLtest_1_vertex_color - tests glVertex* and glColor* functions GLtest_2_rasterpos_bitmap - tests glBitmap and glRasterPos* functions
    Added FUNCTIONS_TESTED constant to each test class, that is used as 1)
    documentation of what is tested 2) for checking whether the test is
    supported on current machine - if not, it is skipped and counted as
    'unsupported'
    
    Previous commit - fixes for glBitmap function and glRasterPos*v exports 
    
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ruby-opengl/trunk@158 0bf6db90-c31b-0410-8265-cbe0ef131713

--- test/interactive/gltest_glut.rb
+++ test/interactive/gltest_glut.rb
@@ -13,13 +13,6 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-# TODO: test if this is working
-if __FILE__ == $0
-    # If we are being called from the command line, e.g., ruby foo.rb, then
-    # fixup the load path so we can find the OpenGL extension libs
-    $: << File.join(File.dirname(__FILE__), '..', 'ext')
-end
-
 require 'gl'
 require 'glut'
 
@@ -96,9 +89,6 @@ module GL_func
 		pop_modelview
 	end
 
-	# text: "Test: testname"
-	# text: "Does this test look like reference rendering (y/n) ?"
-
 	# creates checker RGB bitmap
 	# for RGBA, pass both colors as [r,g,b,a], this applies to other modes as well
 	def checker_texture_bitmap(size=64,divide=8,color_a=[255,255,255],color_b=[0,0,0])
@@ -107,6 +97,29 @@ module GL_func
 		line_strip_a = (strip_a + strip_b) * (size/2)
 		line_strip_b = (strip_b + strip_a) * (size/2)
 		(line_strip_a + line_strip_b) * (divide/2)
+	end
+
+	# creates 'fly' bitmap from OpenGL red book
+	def redbook_fly_bitmap
+		bitmap = [
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x03, 0x80, 0x01, 0xC0, 0x06, 0xC0, 0x03, 0x60,
+0x04, 0x60, 0x06, 0x20, 0x04, 0x30, 0x0C, 0x20,
+0x04, 0x18, 0x18, 0x20, 0x04, 0x0C, 0x30, 0x20,
+0x04, 0x06, 0x60, 0x20, 0x44, 0x03, 0xC0, 0x22,
+0x44, 0x01, 0x80, 0x22, 0x44, 0x01, 0x80, 0x22,
+0x44, 0x01, 0x80, 0x22, 0x44, 0x01, 0x80, 0x22,
+0x44, 0x01, 0x80, 0x22, 0x44, 0x01, 0x80, 0x22,
+0x66, 0x01, 0x80, 0x66, 0x33, 0x01, 0x80, 0xCC,
+0x19, 0x81, 0x81, 0x98, 0x0C, 0xC1, 0x83, 0x30,
+0x07, 0xe1, 0x87, 0xe0, 0x03, 0x3f, 0xfc, 0xc0,
+0x03, 0x31, 0x8c, 0xc0, 0x03, 0x33, 0xcc, 0xc0,
+0x06, 0x64, 0x26, 0x60, 0x0c, 0xcc, 0x33, 0x30,
+0x18, 0xcc, 0x33, 0x18, 0x10, 0xc4, 0x23, 0x08,
+0x10, 0x63, 0xC6, 0x08, 0x10, 0x30, 0x0c, 0x08,
+0x10, 0x18, 0x18, 0x08, 0x10, 0x00, 0x00, 0x08
+		]
+		bitmap.pack("c*")
 	end
 end
 
@@ -141,37 +154,67 @@ class Test_Runner
 
 	def print_summary
 		puts "Test results:"
-		total = pass = fail = 0
+		total = pass = fail = skip = 0
 		@test_list.size.times do |i|
 			total += 1
-			if @test_results[i] == true
+			case @test_results[i]
+			when true:
 				pass += 1
-			else
+			when false:
 				fail += 1
 				puts "#{@test_list[i]} test failed"
+			when nil:
+				skip += 1
+				puts "#{@test_list[i]} test skipped"
 			end
 		end
-		puts "#{total} tests, #{pass} passed, #{fail} failed"
+		puts "#{total} tests, #{pass} passed, #{fail} failed, #{skip} unsupported"
 	end
 
 	def reset_state
 		clear_screen_and_depth_buffer
 	end
 
-	# advances to next test, if any	
-	def next_test
-		if (@test_index == nil)
-			@test_index = 0
-		else
-			@test_index += 1
-			@test.destroy if @test.respond_to? :destroy
-			reset_state
+	def check_functions(c)
+		status = true
+		c::FUNCTIONS_TESTED.each do |f|
+			if Gl.is_available?(f)==false # avaiable on current h/w ?
+				puts "Function #{f} not supported on current machine"
+				status = false
+			end
+			if Gl.methods.include?(f)==false # implemented by our bindings ?
+				puts "Function #{f} not exported by bindings"
+				status = false
+			end
 		end
+		return status
+	end
 
-		if @test_index >= @test_list.size
-			@test = nil
-		else
-			@test = eval(@test_list[@test_index]).new
+	# advances to next test, if any
+	def next_test
+		while true
+			if (@test_index == nil) # first test
+				@test = nil
+				@test_index = 0
+			else # cleanup after previous test (if any) and advance iterator
+				@test.destroy if @test and @test.respond_to? :destroy
+				reset_state if @test
+				@test_index += 1
+			end
+
+			if @test_index >= @test_list.size # beyond last test
+				print_summary	
+				exit
+			end
+
+			cl = eval(@test_list[@test_index]) # String -> class
+			if check_functions(cl) # check if tested functions are supported on current machine
+				@test = cl.new
+				break
+			else # not supported, try next test
+				@test_results[@test_index] = nil
+				@test = nil
+			end
 		end
 	end
 
@@ -199,16 +242,10 @@ class Test_Runner
 		when ?y, ?n
 			@test_results[@test_index] = (k==?y ? true : false)
 			next_test
-			if (@test==nil)
-				print_summary	
-				exit
-			end
 		when 27 # Esc
 			puts "Aborted!"
 	        exit
 	    end
 	    Glut.glutPostRedisplay()
 	end
-
-	# Glut.glutGet(Glut::GLUT_ELAPSED_TIME)
 end
--- test/interactive/tc_base.rb
+++ test/interactive/tc_base.rb
@@ -16,35 +16,337 @@
 require 'gltest_glut'
 
 include GL_func
+include Gl
+include Glut
 
-class GLtest_1
+# tests glVertex* and glColor* functions
+class GLtest_1_vertex_color
+	FUNCTIONS_TESTED = [
+"glBegin","glEnd",
+"glColor3b","glColor3bv","glColor3d","glColor3dv","glColor3f","glColor3fv","glColor3i",
+"glColor3iv","glColor3s","glColor3sv","glColor3ub","glColor3ubv","glColor3ui","glColor3uiv",
+"glColor3us","glColor3usv","glColor4b","glColor4bv","glColor4d","glColor4dv","glColor4f",
+"glColor4fv","glColor4i","glColor4iv","glColor4s","glColor4sv","glColor4ub","glColor4ubv",
+"glColor4ui","glColor4uiv","glColor4us","glColor4usv","glVertex2d","glVertex2dv","glVertex2f",
+"glVertex2fv","glVertex2i","glVertex2iv","glVertex2s","glVertex2sv","glVertex3d","glVertex3dv",
+"glVertex3f","glVertex3fv","glVertex3i","glVertex3iv","glVertex3s","glVertex3sv","glVertex4d",
+"glVertex4dv","glVertex4f","glVertex4fv","glVertex4i","glVertex4iv","glVertex4s","glVertex4sv"
+	]
 	def initialize
-		projection_ortho_box(5)
+		projection_ortho_box(10)
+	end
+	def advance_down
+		glTranslatef(0,@space * -1,0)
+	end
+	def advance_up
+		glTranslatef(0,@space * 1,0)
+	end
+	def advance_right
+		glTranslatef(@space * 1,0,0)
+	end
+	def plain_quad
+		glBegin(GL_QUADS)
+		glVertex2i(-1,-1)
+		glVertex2i( 1,-1)
+		glVertex2i( 1, 1)
+		glVertex2i(-1, 1)
+		glEnd
 	end
 	def loop
+		@space = 2.2
+
+		max_byte = 2**7-1
+		max_ubyte = 2**8-1
+		max_short = 2**15-1
+		max_ushort = 2**16-1
+		max_int = 2**31-1
+		max_uint = 2**32-1
+
 		clear_screen_and_depth_buffer
 		reset_modelview
 
-		Gl.glColor3f(1,0,0)
+		# 1st column
+		glColor3i(0,0,0)
+		glTranslated(@space * -4 +1,-1,0)
 
-		Gl.glBegin(Gl::GL_QUADS)
-		Gl.glVertex3f(-1,-1,0)
-		Gl.glVertex3d( 1,-1,0)
-		Gl.glVertex3f( 1, 1,0)
-		Gl.glVertex3f(-1, 1,0)
-		Gl.glEnd
+		glColor3b(max_byte,max_byte,0)
+
+		glBegin(GL_QUADS)
+		glVertex2s(-1,-1)
+		glVertex2i( 1,-1)
+		glVertex2f( 1.0,1.0)
+		glVertex2d(-1.0,1.0)
+		glEnd
+
+		advance_up
+
+		glColor3s(0,0,max_short)
+
+		glBegin(GL_QUADS)
+		glVertex3s(-1,-1,0)
+		glVertex3i( 1,-1,0)
+		glVertex3f( 1.0,1.0,0.0)
+		glVertex3d(-1.0,1.0,0.0)
+		glEnd
+
+		advance_up
+
+		glColor3i(0,max_int,0)
+
+		glBegin(GL_QUADS)
+		glVertex4s(-1,-1,0,1)
+		glVertex4i( 1,-1,0,1)
+		glVertex4f( 1.0,1.0,0.0,1.0)
+		glVertex4d(-1.0,1.0,0.0,1.0)
+		glEnd
+
+		advance_up
+
+		glColor3f(1.0,0.0,0.0)
+
+		glBegin(GL_QUADS)
+		glVertex2sv([-1,-1])
+		glVertex2iv([ 1,-1])
+		glVertex2fv([ 1.0,1.0])
+		glVertex2dv([-1.0,1.0])
+		glEnd
+
+		# 2nd column
+		glColor3i(0,0,0)
+		advance_right
+
+		glColor3d(1.0,0.0,0.0)
+
+		glBegin(GL_QUADS)
+		glVertex3sv([-1,-1,0])
+		glVertex3iv([ 1,-1,0])
+		glVertex3fv([ 1.0,1.0,0.0])
+		glVertex3dv([-1.0,1.0,0.0])
+		glEnd
+
+		advance_down
+
+		glColor3ub(0,max_ubyte,0)
+
+		glBegin(GL_QUADS)
+		glVertex4sv([-1,-1,0,1])
+		glVertex4iv([ 1,-1,0,1])
+		glVertex4fv([ 1.0,1.0,0.0,1.0])
+		glVertex4dv([-1.0,1.0,0.0,1.0])
+		glEnd
+
+		advance_down
+		glColor3us(0,0,max_ushort)
+		plain_quad
+
+		advance_down
+		glColor3ui(max_uint,max_uint,0)
+		plain_quad
+
+		# 3rd column
+		glColor3i(0,0,0)
+		advance_right
+		glColor4b(max_byte,max_byte,0,max_byte)
+		plain_quad
+
+		advance_up
+		glColor4s(0,0,max_short,max_short)
+		plain_quad
+
+		advance_up
+		glColor4i(0,max_int,0,max_int)
+		plain_quad
+
+		advance_up
+		glColor4f(1.0,0,0,1.0)
+		plain_quad
+
+		# 4th column
+		glColor3i(0,0,0)
+		advance_right
+		glColor4d(1.0,0,0,1.0)
+		plain_quad
+
+		advance_down
+		glColor4ub(0,max_ubyte,0,max_ubyte)
+		plain_quad
+
+		advance_down
+		glColor4us(0,0,max_ushort,max_ushort)
+		plain_quad
+
+		advance_down
+		glColor4ui(max_uint,max_uint,0,max_ubyte)
+		plain_quad
+
+		# 5th column
+		glColor3i(0,0,0)
+		advance_right
+		glColor3bv([max_byte,max_byte,0])
+		plain_quad
+
+		advance_up
+		glColor3sv([0,0,max_short])
+		plain_quad
+	
+		advance_up
+		glColor3iv([0,max_int,0])
+		plain_quad
+
+		advance_up
+		glColor3fv([1.0,0.0,0.0])
+		plain_quad
+
+		# 6th column
+		glColor3i(0,0,0)
+		advance_right
+		glColor3dv([1.0,0.0,0.0])
+		plain_quad
+
+		advance_down
+		glColor3ubv([0,max_ubyte,0])
+		plain_quad
+
+		advance_down
+		glColor3usv([0,0,max_ushort])
+		plain_quad
+
+		advance_down
+		glColor3uiv([max_uint,max_uint,0])
+		plain_quad
+
+		# 7th column
+		glColor3i(0,0,0)
+		advance_right
+		glColor4bv([max_byte,max_byte,0,max_byte])
+		plain_quad
+
+		advance_up
+		glColor4sv([0,0,max_short,max_short])
+		plain_quad
+
+		advance_up
+		glColor4iv([0,max_int,0,max_int])
+		plain_quad
+
+		advance_up
+		glColor4fv([1.0,0.0,0.0,1.0])
+		plain_quad
+
+		# 8th column
+		glColor3i(0,0,0)
+		advance_right
+		glColor4dv([1.0,0.0,0.0,1.0])
+		plain_quad
+
+		advance_down
+		glColor4ubv([0,max_ubyte,0,max_ubyte])
+		plain_quad
+
+		advance_down
+		glColor4usv([0,0,max_ushort,max_ushort])
+		plain_quad
+
+		advance_down
+		glColor4uiv([max_uint,max_uint,0,max_uint])
+		plain_quad
 	end
+
 	def destroy
 	end
 end
 
-class GLtest_2
+# tests glRasterPos* and glBitmap functions
+class GLtest_2_rasterpos_bitmap
+	FUNCTIONS_TESTED = [
+"glBitmap",
+"glRasterPos2d","glRasterPos2dv","glRasterPos2f","glRasterPos2fv",
+"glRasterPos2i","glRasterPos2iv","glRasterPos2s","glRasterPos2sv",
+"glRasterPos3d","glRasterPos3dv","glRasterPos3f","glRasterPos3fv",
+"glRasterPos3i","glRasterPos3iv","glRasterPos3s","glRasterPos3sv",
+"glRasterPos4d","glRasterPos4dv","glRasterPos4f","glRasterPos4fv",
+"glRasterPos4i","glRasterPos4iv","glRasterPos4s","glRasterPos4sv"
+	]
+
 	def initialize
-		projection_ortho_box
+		@bitmap = redbook_fly_bitmap * 4
+		projection_ortho_window
+		@y_pos = 200
+		@x_pos = 55
+		@bmp_h = 32 * 4
+		@bmp_w = 32
+	end
+	def draw_fly_bitmap
+		glBitmap(32,32*4,0,0,0,0,@bitmap)
 	end
 	def loop
 		clear_screen_and_depth_buffer
-		print_text("Hello",256,256,[0,255,0],Glut::GLUT_BITMAP_TIMES_ROMAN_24)
+
+		glColor3f(1.0,1.0,1.0)
+
+		# 2*
+
+		glRasterPos2d(@x_pos,@y_pos)
+		draw_fly_bitmap
+		glRasterPos2dv([@x_pos,@y_pos+@bmp_h])
+		draw_fly_bitmap
+
+		glRasterPos2f(@x_pos+@bmp_w,@y_pos)
+		draw_fly_bitmap
+		glRasterPos2fv([@x_pos+@bmp_w,@y_pos+@bmp_h])
+		draw_fly_bitmap
+
+		glRasterPos2i(@x_pos+@bmp_w*2,@y_pos)
+		draw_fly_bitmap
+		glRasterPos2iv([@x_pos+@bmp_w*2,@y_pos+@bmp_h])
+		draw_fly_bitmap
+
+		glRasterPos2s(@x_pos+@bmp_w*3,@y_pos)
+		draw_fly_bitmap
+		glRasterPos2sv([@x_pos+@bmp_w*3,@y_pos+@bmp_h])
+		draw_fly_bitmap
+
+		# 3*
+		glRasterPos3d(@x_pos+@bmp_w*4,@y_pos,0)
+		draw_fly_bitmap
+		glRasterPos3dv([@x_pos+@bmp_w*4,@y_pos+@bmp_h,0])
+		draw_fly_bitmap
+
+		glRasterPos3f(@x_pos+@bmp_w*5,@y_pos,0)
+		draw_fly_bitmap
+		glRasterPos3fv([@x_pos+@bmp_w*5,@y_pos+@bmp_h,0])
+		draw_fly_bitmap
+
+		glRasterPos3i(@x_pos+@bmp_w*6,@y_pos,0)
+		draw_fly_bitmap
+		glRasterPos3iv([@x_pos+@bmp_w*6,@y_pos+@bmp_h,0])
+		draw_fly_bitmap
+
+		glRasterPos3s(@x_pos+@bmp_w*7,@y_pos,0)
+		draw_fly_bitmap
+		glRasterPos3sv([@x_pos+@bmp_w*7,@y_pos+@bmp_h,0])
+		draw_fly_bitmap
+
+		# 4*
+		glRasterPos4d(@x_pos+@bmp_w*8,@y_pos,0,1)
+		draw_fly_bitmap
+		glRasterPos4dv([@x_pos+@bmp_w*8,@y_pos+@bmp_h,0,1])
+		draw_fly_bitmap
+
+		glRasterPos4f(@x_pos+@bmp_w*9,@y_pos,0,1)
+		draw_fly_bitmap
+		glRasterPos4fv([@x_pos+@bmp_w*9,@y_pos+@bmp_h,0,1])
+		draw_fly_bitmap
+
+		glRasterPos4i(@x_pos+@bmp_w*10,@y_pos,0,1)
+		draw_fly_bitmap
+		glRasterPos4iv([@x_pos+@bmp_w*10,@y_pos+@bmp_h,0,1])
+		draw_fly_bitmap
+
+		glRasterPos4s(@x_pos+@bmp_w*11,@y_pos,0,1)
+		draw_fly_bitmap
+		glRasterPos4sv([@x_pos+@bmp_w*11,@y_pos+@bmp_h,0,1])
+		draw_fly_bitmap
 	end
 	def destroy
 	end
