3c243de5f20e2e1e0cea48f508973cb6a8d3d182
Eric Hodel <drbrain@segment7.net>
2011-12-16 10:12:07 -0800

    Clean up gl-ext-ati tests

--- test/test_gl_ext_ati.rb
+++ test/test_gl_ext_ati.rb
@@ -16,18 +16,15 @@
 require 'opengl/test_case'
 
 class TestGlExtAti < OpenGL::TestCase
-	def setup
-		super()
-	end
 
-	def teardown
-		super()
-	end
+  def test_gl_ati_draw_buffers
+    supported? "GL_ATI_draw_buffers"
 
-	def test_gl_ati_draw_buffers
-		supported?("GL_ATI_draw_buffers")
-		glDrawBuffersATI([GL_BACK_LEFT,GL_FRONT_LEFT])
-		assert_equal(glGetIntegerv(GL_DRAW_BUFFER0),GL_BACK_LEFT)
-		assert_equal(glGetIntegerv(GL_DRAW_BUFFER1),GL_FRONT_LEFT)
-	end
+    glDrawBuffersATI [GL_BACK_LEFT, GL_FRONT_LEFT]
+
+    assert_equal GL_BACK_LEFT, glGetIntegerv(GL_DRAW_BUFFER0)
+    assert_equal GL_FRONT_LEFT, glGetIntegerv(GL_DRAW_BUFFER1)
+  end
+
 end
+
