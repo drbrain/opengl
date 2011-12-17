625d0510215c1060cb1c634c969a8b2e53dff050
Eric Hodel <drbrain@segment7.net>
2011-12-16 16:20:26 -0800

    Reindent

--- examples/NeHe/nehe_lesson02.rb
+++ examples/NeHe/nehe_lesson02.rb
@@ -2,7 +2,7 @@
 # Name:  nehe_lesson02.rb
 # Purpose: An implementation of NeHe's OpenGL Lesson #02
 #          using ruby-opengl (http://nehe.gamedev.net/)
-# 
+#
 
 require "gl"
 require "glu"
@@ -18,87 +18,87 @@ include Glut
 window = ""
 
 def init_gl_window(width = 640, height = 480)
-    # Background color to black
-    glClearColor(0.0, 0.0, 0.0, 0)
-    # Enables clearing of depth buffer
-    glClearDepth(1.0)
-    # Set type of depth test
-    glDepthFunc(GL_LEQUAL)
-    # Enable depth testing
-    glEnable(GL_DEPTH_TEST)
-    # Enable smooth color shading
-    glShadeModel(GL_SMOOTH)
+  # Background color to black
+  glClearColor(0.0, 0.0, 0.0, 0)
+  # Enables clearing of depth buffer
+  glClearDepth(1.0)
+  # Set type of depth test
+  glDepthFunc(GL_LEQUAL)
+  # Enable depth testing
+  glEnable(GL_DEPTH_TEST)
+  # Enable smooth color shading
+  glShadeModel(GL_SMOOTH)
 
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity
-    # Calculate aspect ratio of the window
-    gluPerspective(45.0, width / height, 0.1, 100.0)
+  glMatrixMode(GL_PROJECTION)
+  glLoadIdentity
+  # Calculate aspect ratio of the window
+  gluPerspective(45.0, width / height, 0.1, 100.0)
 
-    glMatrixMode(GL_MODELVIEW)
+  glMatrixMode(GL_MODELVIEW)
 
-    draw_gl_scene
+  draw_gl_scene
 end
 
 #reshape = Proc.new do |width, height|
 def reshape(width, height)
-    height = 1 if height == 0
+  height = 1 if height == 0
 
-    # Reset current viewpoint and perspective transformation
-    glViewport(0, 0, width, height)
+  # Reset current viewpoint and perspective transformation
+  glViewport(0, 0, width, height)
 
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity
+  glMatrixMode(GL_PROJECTION)
+  glLoadIdentity
 
-    gluPerspective(45.0, width / height, 0.1, 100.0)
+  gluPerspective(45.0, width / height, 0.1, 100.0)
 end
 
 #draw_gl_scene = Proc.new do
 def draw_gl_scene
-    # Clear the screen and depth buffer
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+  # Clear the screen and depth buffer
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    # Reset the view
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity
+  # Reset the view
+  glMatrixMode(GL_MODELVIEW)
+  glLoadIdentity
 
-    # Move left 1.5 units and into the screen 6.0 units
-    glTranslatef(-1.5, 0.0, -6.0)
+  # Move left 1.5 units and into the screen 6.0 units
+  glTranslatef(-1.5, 0.0, -6.0)
 
-    # Draw a triangle
-    glBegin(GL_POLYGON)
-        glVertex3f( 0.0,  1.0, 0.0)
-        glVertex3f( 1.0, -1.0, 0.0)
-        glVertex3f(-1.0, -1.0, 0.0)
-    glEnd
+  # Draw a triangle
+  glBegin(GL_POLYGON)
+  glVertex3f( 0.0,  1.0, 0.0)
+  glVertex3f( 1.0, -1.0, 0.0)
+  glVertex3f(-1.0, -1.0, 0.0)
+  glEnd
 
-    # Move right 3 units
-    glTranslatef(3.0, 0.0, 0.0)
+  # Move right 3 units
+  glTranslatef(3.0, 0.0, 0.0)
 
-    # Draw a rectangle
-    glBegin(GL_QUADS)
-        glVertex3f(-1.0,  1.0, 0.0)
-        glVertex3f( 1.0,  1.0, 0.0)
-        glVertex3f( 1.0, -1.0, 0.0)
-        glVertex3f(-1.0, -1.0, 0.0)
-    glEnd
+  # Draw a rectangle
+  glBegin(GL_QUADS)
+  glVertex3f(-1.0,  1.0, 0.0)
+  glVertex3f( 1.0,  1.0, 0.0)
+  glVertex3f( 1.0, -1.0, 0.0)
+  glVertex3f(-1.0, -1.0, 0.0)
+  glEnd
 
-    # Swap buffers for display 
-    glutSwapBuffers
+  # Swap buffers for display 
+  glutSwapBuffers
 end
 
 # The idle function to handle 
 def idle
-    glutPostRedisplay
+  glutPostRedisplay
 end
 
 # Keyboard handler to exit when ESC is typed
 keyboard = lambda do |key, x, y|
-    case(key)
-      when ?\e
-          glutDestroyWindow(window)
-          exit(0)
-      end
-      glutPostRedisplay
+  case(key)
+  when ?\e
+    glutDestroyWindow(window)
+    exit(0)
+  end
+  glutPostRedisplay
 end
 
 
