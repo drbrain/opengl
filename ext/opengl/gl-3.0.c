68b316a93edc2618c88e862c2567c32811710dab
Blaž Hrastnik <speed.the.bboy@gmail.com>
2013-02-27 15:50:13 +0100

    Add initial OpenGL 3.0 work (moved finished extensions to file).

---
+++ ext/opengl/gl-3.0.c
@@ -0,0 +1,419 @@
+/*
+ * Copyright (C) 2013 Blaž Hrastnik <speed.the.bboy@gmail.com>
+ *
+ * This program is distributed under the terms of the MIT license.
+ * See the included MIT-LICENSE file for the terms of this license.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "common.h"
+
+/* 
+	GL_NV_conditional_render
+	GL_APPLE_flush_buffer_range
+
+	GL_NV_half_float
+
+	GL_EXT_draw_buffers2
+	GL_EXT_transform_feedback
+	GL_APPLE_vertex_array_object
+
+*/
+
+/* #326 - GL_EXT_gpu_shader4 */
+GL_FUNC_LOAD_2(VertexAttribI1i,GLvoid, GLuint,GLint, "3.0")
+GL_FUNC_LOAD_3(VertexAttribI2i,GLvoid, GLuint,GLint,GLint, "3.0")
+GL_FUNC_LOAD_4(VertexAttribI3i,GLvoid, GLuint,GLint,GLint,GLint, "3.0")
+GL_FUNC_LOAD_5(VertexAttribI4i,GLvoid, GLuint,GLint,GLint,GLint,GLint, "3.0")
+GL_FUNC_LOAD_2(VertexAttribI1ui,GLvoid, GLuint,GLuint, "3.0")
+GL_FUNC_LOAD_3(VertexAttribI2ui,GLvoid, GLuint,GLuint,GLuint, "3.0")
+GL_FUNC_LOAD_4(VertexAttribI3ui,GLvoid, GLuint,GLuint,GLuint,GLuint, "3.0")
+GL_FUNC_LOAD_5(VertexAttribI4ui,GLvoid, GLuint,GLuint,GLuint,GLuint,GLuint, "3.0")
+
+#define GLVERTEXATTRIB_VFUNC(_name_,_type_,_conv_,_size_) \
+static void (APIENTRY * fptr_gl##_name_)(GLuint,const _type_ *); \
+static VALUE \
+gl_##_name_(obj,arg1,arg2) \
+VALUE obj,arg1,arg2; \
+{ \
+	_type_ value[_size_]; \
+	LOAD_GL_FUNC(gl##_name_, "3.0"); \
+	_conv_(arg2,value,_size_); \
+	fptr_gl##_name_(NUM2UINT(arg1),value); \
+	CHECK_GLERROR_FROM("gl" #_name_); \
+	return Qnil; \
+}
+
+GLVERTEXATTRIB_VFUNC(VertexAttribI1iv,GLint,ary2cint,1)
+GLVERTEXATTRIB_VFUNC(VertexAttribI2iv,GLint,ary2cint,2)
+GLVERTEXATTRIB_VFUNC(VertexAttribI3iv,GLint,ary2cint,3)
+GLVERTEXATTRIB_VFUNC(VertexAttribI4iv,GLint,ary2cint,4)
+GLVERTEXATTRIB_VFUNC(VertexAttribI1uiv,GLuint,ary2cuint,1)
+GLVERTEXATTRIB_VFUNC(VertexAttribI2uiv,GLuint,ary2cuint,2)
+GLVERTEXATTRIB_VFUNC(VertexAttribI3uiv,GLuint,ary2cuint,3)
+GLVERTEXATTRIB_VFUNC(VertexAttribI4uiv,GLuint,ary2cuint,4)
+GLVERTEXATTRIB_VFUNC(VertexAttribI4bv,GLbyte,ary2cbyte,4)
+GLVERTEXATTRIB_VFUNC(VertexAttribI4sv,GLshort,ary2cshort,4)
+GLVERTEXATTRIB_VFUNC(VertexAttribI4ubv,GLubyte,ary2cubyte,4)
+GLVERTEXATTRIB_VFUNC(VertexAttribI4usv,GLushort,ary2cushort,4)
+#undef GLVERTEXATTRIB_VFUNC
+
+#define GETVERTEXATTRIB_FUNC(_name_,_type_,_conv_,_extension_) \
+static void (APIENTRY * fptr_gl##_name_)(GLuint,GLenum,_type_ *); \
+static VALUE \
+gl_##_name_(obj,arg1,arg2) \
+VALUE obj,arg1,arg2; \
+{ \
+	GLuint index; \
+	GLenum pname; \
+	_type_ params[4] = {0,0,0,0}; \
+	GLint size; \
+	LOAD_GL_FUNC(gl##_name_, _extension_); \
+	index = (GLuint)NUM2UINT(arg1); \
+	pname = (GLenum)NUM2INT(arg2); \
+	if (pname==GL_CURRENT_VERTEX_ATTRIB_ARB) \
+		size = 4; \
+	else \
+		size = 1; \
+	fptr_gl##_name_(index,pname,params); \
+	RET_ARRAY_OR_SINGLE_BOOL("gl" #_name_, size, _conv_, pname, params); \
+}
+
+GETVERTEXATTRIB_FUNC(GetVertexAttribIiv,GLint,cond_GLBOOL2RUBY,"3.0")
+GETVERTEXATTRIB_FUNC(GetVertexAttribIuiv,GLuint,cond_GLBOOL2RUBY_U,"3.0")
+#undef GETVERTEXATTRIB_FUNC
+
+extern VALUE g_VertexAttrib_ptr[];
+
+static void (APIENTRY * fptr_glVertexAttribIPointer)(GLuint,GLint,GLenum,GLsizei,const GLvoid *);
+static VALUE gl_VertexAttribIPointer(VALUE obj,VALUE arg1,VALUE arg2,VALUE arg3,VALUE arg4,VALUE arg5)
+{
+	GLuint index;
+	GLuint size;
+	GLenum type;
+	GLsizei stride;
+
+	LOAD_GL_FUNC(glVertexAttribIPointer, "3.0");
+
+	index = (GLuint)NUM2UINT(arg1);
+	size = (GLuint)NUM2UINT(arg2);
+	type = (GLenum)NUM2INT(arg3);
+	stride = (GLsizei)NUM2UINT(arg4);
+	if (index>_MAX_VERTEX_ATTRIBS)
+		rb_raise(rb_eArgError, "Index too large, maximum allowed value '%i'",_MAX_VERTEX_ATTRIBS);
+
+	if (CheckBufferBinding(GL_ARRAY_BUFFER_BINDING)) {
+		g_VertexAttrib_ptr[index] = arg5;
+		fptr_glVertexAttribIPointer(index,size,type,stride,(GLvoid *)NUM2LONG(arg5));
+	} else {
+		VALUE data;
+		data = pack_array_or_pass_string(type,arg5);
+		rb_str_freeze(data);
+		g_VertexAttrib_ptr[index] = data;
+		fptr_glVertexAttribIPointer(index,size,type,stride,(GLvoid *)RSTRING_PTR(data));
+	}
+
+	CHECK_GLERROR_FROM("glVertexAttribIPointer");
+	return Qnil;
+}
+
+GL_FUNC_LOAD_2(Uniform1ui,GLvoid, GLint,GLuint, "3.0")
+GL_FUNC_LOAD_3(Uniform2ui,GLvoid, GLint,GLuint,GLuint, "3.0")
+GL_FUNC_LOAD_4(Uniform3ui,GLvoid, GLint,GLuint,GLuint,GLuint, "3.0")
+GL_FUNC_LOAD_5(Uniform4ui,GLvoid, GLint,GLuint,GLuint,GLuint,GLuint, "3.0")
+
+#define GLUNIFORM_VFUNC(_name_,_type_,_conv_,_size_) \
+static void (APIENTRY * fptr_gl##_name_)(GLint,GLsizei,const _type_ *); \
+static VALUE \
+gl_##_name_(obj,arg1,arg2) \
+VALUE obj,arg1,arg2; \
+{ \
+	GLint location; \
+	GLsizei count; \
+	_type_ *value; \
+	LOAD_GL_FUNC(gl##_name_, "3.0"); \
+	Check_Type(arg2,T_ARRAY); \
+	count = (GLsizei)RARRAY_LENINT(arg2); \
+	if (count<=0 || (count % _size_) != 0) \
+		rb_raise(rb_eArgError, "Parameter array size must be multiplication of %i",_size_); \
+	location = (GLint)NUM2INT(arg1); \
+	value = ALLOC_N(_type_,count); \
+	_conv_(arg2,value,count); \
+	fptr_gl##_name_(location,count / _size_,value); \
+	xfree(value); \
+	CHECK_GLERROR_FROM("gl" #_name_); \
+	return Qnil; \
+}
+
+GLUNIFORM_VFUNC(Uniform1uiv,GLuint,ary2cuint,1)
+GLUNIFORM_VFUNC(Uniform2uiv,GLuint,ary2cuint,2)
+GLUNIFORM_VFUNC(Uniform3uiv,GLuint,ary2cuint,3)
+GLUNIFORM_VFUNC(Uniform4uiv,GLuint,ary2cuint,4)
+#undef GLUNIFORM_VFUNC
+
+static void (APIENTRY * fptr_glGetActiveUniformARB)(GLuint,GLuint,GLsizei,GLsizei*,GLint*,GLenum*,GLchar*);
+#define GETUNIFORM_FUNC(_name_,_type_) \
+static void (APIENTRY * fptr_gl##_name_)(GLuint,GLint,_type_ *); \
+static VALUE \
+gl_##_name_(obj,arg1,arg2) \
+VALUE obj,arg1,arg2; \
+{ \
+	GLuint program; \
+	GLint location; \
+	_type_ params[16]; \
+	GLint unused = 0; \
+	GLenum uniform_type = 0; \
+	GLint uniform_size = 0; \
+\
+	LOAD_GL_FUNC(gl##_name_, "3.0"); \
+	LOAD_GL_FUNC(glGetActiveUniformARB, "3.0"); \
+	program = (GLuint)NUM2UINT(arg1); \
+	location = (GLint)NUM2INT(arg2); \
+\
+	fptr_glGetActiveUniformARB(program,location,0,NULL,&unused,&uniform_type,NULL); \
+	CHECK_GLERROR_FROM("glGetActiveUniformARB"); \
+	if (uniform_type==0) \
+		rb_raise(rb_eTypeError, "Can't determine the uniform's type"); \
+\
+	uniform_size = get_uniform_size(uniform_type); \
+\
+	memset(params,0,16*sizeof(_type_)); \
+	fptr_gl##_name_(program,location,params); \
+	RET_ARRAY_OR_SINGLE("gl" #_name_, uniform_size, RETCONV_##_type_, params); \
+}
+
+GETUNIFORM_FUNC(GetUniformuiv,GLuint)
+#undef GETUNIFORM_FUNC
+
+static void (APIENTRY * fptr_glBindFragDataLocation)(GLuint,GLuint,const GLchar *);
+static VALUE gl_BindFragDataLocation(VALUE obj,VALUE arg1,VALUE arg2,VALUE arg3)
+{
+	LOAD_GL_FUNC(glBindFragDataLocation, "3.0");
+	Check_Type(arg3,T_STRING);
+	fptr_glBindFragDataLocation(NUM2UINT(arg1),NUM2UINT(arg2),RSTRING_PTR(arg3));
+	CHECK_GLERROR_FROM("glBindFragDataLocation");
+	return Qnil;
+}
+
+static GLint (APIENTRY * fptr_glGetFragDataLocation)(GLuint,const GLchar *);
+static VALUE gl_GetFragDataLocation(VALUE obj,VALUE arg1,VALUE arg2)
+{
+	GLint ret;
+	LOAD_GL_FUNC(glGetFragDataLocation, "3.0");
+	Check_Type(arg2,T_STRING);
+	ret = fptr_glGetFragDataLocation(NUM2UINT(arg1),RSTRING_PTR(arg2));
+	CHECK_GLERROR_FROM("glGetFragDataLocation");
+	return INT2NUM(ret);
+}
+
+/* GL_NV_conditional_render */
+
+/* GL_APPLE_flush_buffer_range */
+
+/* #39 GL_ARB_color_buffer_float */
+
+GL_FUNC_LOAD_2(ClampColor,GLvoid, GLenum,GLenum, "3.0")
+
+/* #334 GL_NV_depth_buffer_float */
+
+GL_FUNC_LOAD_2(DepthRanged,GLvoid, GLdouble,GLdouble, "3.0")
+GL_FUNC_LOAD_1(ClearDepthd,GLvoid, GLdouble, "3.0")
+GL_FUNC_LOAD_2(DepthBoundsd,GLvoid, GLdouble,GLdouble, "3.0")
+
+/* #310 - GL_EXT_framebuffer_object */
+GL_FUNC_LOAD_1(IsRenderbuffer,GLboolean, GLuint, "3.0")
+GL_FUNC_LOAD_2(BindRenderbuffer,GLvoid, GLenum,GLuint, "3.0")
+GL_FUNC_LOAD_4(RenderbufferStorage,GLvoid, GLenum,GLenum,GLsizei,GLsizei, "3.0")
+GL_FUNC_GENOBJECTS_LOAD(GenRenderbuffers,"3.0")
+GL_FUNC_DELETEOBJECTS_LOAD(DeleteRenderbuffers,"3.0")
+
+static void (APIENTRY * fptr_glGetRenderbufferParameteriv)(GLenum,GLenum,GLint *);
+static VALUE gl_GetRenderbufferParameteriv(VALUE obj,VALUE arg1,VALUE arg2)
+{
+	GLint param = 0;
+	LOAD_GL_FUNC(glGetRenderbufferParameteriv, "3.0");
+	fptr_glGetRenderbufferParameteriv(NUM2UINT(arg1),NUM2UINT(arg2),&param);
+	CHECK_GLERROR_FROM("glGetRenderbufferParameteriv");
+	return INT2NUM(param);
+}
+
+GL_FUNC_GENOBJECTS_LOAD(GenFramebuffers,"3.0")
+GL_FUNC_DELETEOBJECTS_LOAD(DeleteFramebuffers,"3.0")
+GL_FUNC_LOAD_1(IsFramebuffer,GLboolean, GLuint, "3.0")
+GL_FUNC_LOAD_2(BindFramebuffer,GLvoid, GLenum,GLuint, "3.0")
+GL_FUNC_LOAD_1(CheckFramebufferStatus,GLenum, GLenum, "3.0")
+GL_FUNC_LOAD_5(FramebufferTexture1D,GLvoid, GLenum,GLenum,GLenum,GLuint,GLint, "3.0")
+GL_FUNC_LOAD_5(FramebufferTexture2D,GLvoid, GLenum,GLenum,GLenum,GLuint,GLint, "3.0")
+GL_FUNC_LOAD_6(FramebufferTexture3D,GLvoid, GLenum,GLenum,GLenum,GLuint,GLint,GLint, "3.0")
+
+GL_FUNC_LOAD_4(FramebufferRenderbuffer,GLvoid, GLuint,GLuint,GLuint,GLuint, "3.0")
+
+static void (APIENTRY * fptr_glGetFramebufferAttachmentParameteriv)(GLenum,GLenum,GLenum,GLint *);
+static VALUE gl_GetFramebufferAttachmentParameteriv(VALUE obj,VALUE arg1, VALUE arg2, VALUE arg3)
+{
+	GLint ret = 0;
+	LOAD_GL_FUNC(glGetFramebufferAttachmentParameteriv, "3.0");
+	fptr_glGetFramebufferAttachmentParameteriv(NUM2UINT(arg1),NUM2UINT(arg2),NUM2UINT(arg3),&ret);
+	CHECK_GLERROR_FROM("glGetFramebufferAttachmentParameteriv");
+	return cond_GLBOOL2RUBY(NUM2UINT(arg3),ret);
+}
+
+GL_FUNC_LOAD_1(GenerateMipmap,GLvoid, GLenum, "3.0")
+
+/* GL_NV_half_float */
+
+/* #317 - GL_EXT_framebuffer_multisample */
+GL_FUNC_LOAD_5(RenderbufferStorageMultisample,GLvoid, GLenum,GLsizei,GLenum,GLsizei,GLsizei, "3.0")
+
+/* #316 - GL_EXT_framebuffer_blit */
+GL_FUNC_LOAD_10(BlitFramebuffer,GLvoid, GLint,GLint,GLint,GLint,GLint,GLint,GLint,GLint,GLbitfield,GLenum, "3.0")
+
+/* #343 - GL_EXT_texture_integer */
+GL_FUNC_LOAD_4(ClearColorIi,GLvoid, GLint,GLint,GLint,GLint, "3.0")
+GL_FUNC_LOAD_4(ClearColorIui,GLvoid, GLuint,GLuint,GLuint,GLuint, "3.0")
+
+#define TEXPARAMETER_VFUNC(_name_,_type_,_conv_) \
+static void (APIENTRY * fptr_gl##_name_)(GLenum,GLenum,_type_ *); \
+static VALUE \
+gl_##_name_(obj,arg1,arg2,arg3) \
+VALUE obj,arg1,arg2,arg3; \
+{ \
+	GLenum target; \
+	GLenum pname; \
+	_type_ params[4] = {0,0,0,0}; \
+	LOAD_GL_FUNC(gl##_name_, "3.0"); \
+	target = (GLenum)NUM2UINT(arg1); \
+	pname = (GLenum)NUM2UINT(arg2); \
+	Check_Type(arg3,T_ARRAY); \
+	_conv_(arg3,params,4); \
+	fptr_gl##_name_(target,pname,params); \
+	CHECK_GLERROR_FROM("gl" #_name_); \
+	return Qnil; \
+}
+
+TEXPARAMETER_VFUNC(TexParameterIiv,GLint,ary2cint)
+TEXPARAMETER_VFUNC(TexParameterIuiv,GLuint,ary2cuint)
+#undef TEXPARAMETER_VFUNC
+
+#define GETTEXPARAMETER_VFUNC(_name_,_type_,_conv_) \
+static void (APIENTRY * fptr_gl##_name_)(GLenum,GLenum,_type_ *); \
+static VALUE \
+gl_##_name_(obj,arg1,arg2) \
+VALUE obj,arg1,arg2; \
+{ \
+	GLenum target; \
+	GLenum pname; \
+	_type_ params[4] = {0,0,0,0}; \
+	int size; \
+	LOAD_GL_FUNC(gl##_name_, "3.0"); \
+	target = (GLenum)NUM2INT(arg1); \
+	pname = (GLenum)NUM2INT(arg2); \
+	switch(pname) { \
+		case GL_TEXTURE_BORDER_COLOR: \
+		case GL_TEXTURE_BORDER_VALUES_NV: \
+		case GL_POST_TEXTURE_FILTER_BIAS_SGIX: \
+		case GL_POST_TEXTURE_FILTER_SCALE_SGIX: \
+			size = 4; \
+			break; \
+		default: \
+			size = 1; \
+			break; \
+	} \
+	fptr_gl##_name_(target,pname,params); \
+	RET_ARRAY_OR_SINGLE_BOOL("gl" #_name_, size, _conv_, pname, params); \
+}
+
+GETTEXPARAMETER_VFUNC(GetTexParameterIiv,GLint,cond_GLBOOL2RUBY)
+GETTEXPARAMETER_VFUNC(GetTexParameterIuiv,GLuint,cond_GLBOOL2RUBY_U)
+#undef GETTEXPARAMETER_VFUNC
+
+void gl_init_functions_3_0(VALUE module)
+{
+	/* #326 - GL_EXT_gpu_shader4 */
+	rb_define_module_function(module, "glVertexAttribI1i", gl_VertexAttribI1i, 2);
+	rb_define_module_function(module, "glVertexAttribI2i", gl_VertexAttribI2i, 3);
+	rb_define_module_function(module, "glVertexAttribI3i", gl_VertexAttribI3i, 4);
+	rb_define_module_function(module, "glVertexAttribI4i", gl_VertexAttribI4i, 5);
+	rb_define_module_function(module, "glVertexAttribI1ui", gl_VertexAttribI1ui, 2);
+	rb_define_module_function(module, "glVertexAttribI2ui", gl_VertexAttribI2ui, 3);
+	rb_define_module_function(module, "glVertexAttribI3ui", gl_VertexAttribI3ui, 4);
+	rb_define_module_function(module, "glVertexAttribI4ui", gl_VertexAttribI4ui, 5);
+	rb_define_module_function(module, "glVertexAttribI1iv", gl_VertexAttribI1iv, 2);
+	rb_define_module_function(module, "glVertexAttribI2iv", gl_VertexAttribI2iv, 2);
+	rb_define_module_function(module, "glVertexAttribI3iv", gl_VertexAttribI3iv, 2);
+	rb_define_module_function(module, "glVertexAttribI4iv", gl_VertexAttribI4iv, 2);
+	rb_define_module_function(module, "glVertexAttribI1uiv", gl_VertexAttribI1uiv, 2);
+	rb_define_module_function(module, "glVertexAttribI2uiv", gl_VertexAttribI2uiv, 2);
+	rb_define_module_function(module, "glVertexAttribI3uiv", gl_VertexAttribI3uiv, 2);
+	rb_define_module_function(module, "glVertexAttribI4uiv", gl_VertexAttribI4uiv, 2);
+	rb_define_module_function(module, "glVertexAttribI4bv", gl_VertexAttribI4bv, 2);
+	rb_define_module_function(module, "glVertexAttribI4sv", gl_VertexAttribI4sv, 2);
+	rb_define_module_function(module, "glVertexAttribI4ubv", gl_VertexAttribI4ubv, 2);
+	rb_define_module_function(module, "glVertexAttribI4usv", gl_VertexAttribI4usv, 2);
+	rb_define_module_function(module, "glVertexAttribIPointer", gl_VertexAttribIPointer, 5);
+	rb_define_module_function(module, "glGetVertexAttribIiv", gl_GetVertexAttribIiv, 2);
+	rb_define_module_function(module, "glGetVertexAttribIuiv", gl_GetVertexAttribIuiv, 2);
+	rb_define_module_function(module, "glUniform1ui", gl_Uniform1ui, 2);
+	rb_define_module_function(module, "glUniform2ui", gl_Uniform2ui, 3);
+	rb_define_module_function(module, "glUniform3ui", gl_Uniform3ui, 4);
+	rb_define_module_function(module, "glUniform4ui", gl_Uniform4ui, 5);
+	rb_define_module_function(module, "glUniform1uiv", gl_Uniform1uiv, 2);
+	rb_define_module_function(module, "glUniform2uiv", gl_Uniform2uiv, 2);
+	rb_define_module_function(module, "glUniform3uiv", gl_Uniform3uiv, 2);
+	rb_define_module_function(module, "glUniform4uiv", gl_Uniform4uiv, 2);
+	rb_define_module_function(module, "glGetUniformuiv", gl_GetUniformuiv, 2);
+	rb_define_module_function(module, "glBindFragDataLocation", gl_BindFragDataLocation, 3);
+	rb_define_module_function(module, "glGetFragDataLocation", gl_GetFragDataLocation, 2);
+
+	/* GL_NV_conditional_render */
+
+	/* GL_APPLE_flush_buffer_range */
+
+	/* #39 GL_ARB_color_buffer_float */
+	rb_define_module_function(module, "glClampColor", gl_ClampColor, 2);
+
+	/* #334 GL_NV_depth_buffer_float */
+	rb_define_module_function(module, "glDepthRanged", gl_DepthRanged, 2);
+	rb_define_module_function(module, "glClearDepthd", gl_ClearDepthd, 1);
+	rb_define_module_function(module, "glDepthBoundsd", gl_DepthBoundsd, 2);
+
+	/* #310 - GL_EXT_framebuffer_object */
+	rb_define_module_function(module, "glIsRenderbuffer", gl_IsRenderbuffer, 1);
+	rb_define_module_function(module, "glBindRenderbuffer", gl_BindRenderbuffer, 2);
+	rb_define_module_function(module, "glDeleteRenderbuffers", gl_DeleteRenderbuffers, 1);
+	rb_define_module_function(module, "glGenRenderbuffers", gl_GenRenderbuffers, 1);
+	rb_define_module_function(module, "glRenderbufferStorage", gl_RenderbufferStorage, 4);
+	rb_define_module_function(module, "glGetRenderbufferParameteriv", gl_GetRenderbufferParameteriv, 2);
+	rb_define_module_function(module, "glIsFramebuffer", gl_IsFramebuffer, 1);
+	rb_define_module_function(module, "glBindFramebuffer", gl_BindFramebuffer, 2);
+	rb_define_module_function(module, "glDeleteFramebuffers", gl_DeleteFramebuffers, 1);
+	rb_define_module_function(module, "glGenFramebuffers", gl_GenFramebuffers, 1);
+	rb_define_module_function(module, "glCheckFramebufferStatus", gl_CheckFramebufferStatus, 1);
+	rb_define_module_function(module, "glFramebufferTexture1D", gl_FramebufferTexture1D, 5);
+	rb_define_module_function(module, "glFramebufferTexture2D", gl_FramebufferTexture2D, 5);
+	rb_define_module_function(module, "glFramebufferTexture3D", gl_FramebufferTexture3D, 6);
+	rb_define_module_function(module, "glFramebufferRenderbuffer", gl_FramebufferRenderbuffer, 4);
+	rb_define_module_function(module, "glGetFramebufferAttachmentParameteriv", gl_GetFramebufferAttachmentParameteriv, 3);
+	rb_define_module_function(module, "glGenerateMipmap", gl_GenerateMipmap, 1);
+
+	/* #317 - GL_EXT_framebuffer_multisample */
+	rb_define_module_function(module, "glRenderbufferStorageMultisample", gl_RenderbufferStorageMultisample, 5);
+
+	/* #316 - GL_EXT_framebuffer_blit */
+	rb_define_module_function(module, "glBlitFramebuffer", gl_BlitFramebuffer, 10);
+
+	/* #343 - GL_EXT_texture_integer */
+	rb_define_module_function(module, "glClearColorIi", gl_ClearColorIi, 4);
+	rb_define_module_function(module, "glClearColorIui", gl_ClearColorIui, 4);
+	rb_define_module_function(module, "glTexParameterIiv", gl_TexParameterIiv, 3);
+	rb_define_module_function(module, "glTexParameterIuiv", gl_TexParameterIuiv, 3);
+	rb_define_module_function(module, "glGetTexParameterIiv", gl_GetTexParameterIiv, 2);
+	rb_define_module_function(module, "glGetTexParameterIuiv", gl_GetTexParameterIuiv, 2);
+}
