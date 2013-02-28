8a6ad97e717b5cc2ed82061b554d952a7446e037
Blaž Hrastnik <speed.the.bboy@gmail.com>
2013-02-28 20:25:51 +0100

    Add Glut extended initialization methods (context creation)

--- ext/opengl/opengl.c
+++ ext/opengl/opengl.c
@@ -4,10 +4,13 @@ void Init_gl(void);
 void Init_glu(void);
 void Init_glut(void);
 void Init_glut_callbacks(void);
+void Init_glut_ext(void);
 
 DLLEXPORT void Init_opengl() {
 	Init_gl();
 	Init_glu();
 	Init_glut();
 	Init_glut_callbacks();
+	Init_glut_ext();
+
 }
