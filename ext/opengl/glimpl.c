304bd423518fbe78bfead23c9122232bae3a9fde
Lars Kanis <lars@greiz-reinsdorf.de>
2015-01-20 21:22:02 +0100

    Add dynmic library loader for Windows.

--- ext/opengl/glimpl.c
+++ ext/opengl/glimpl.c
@@ -9,6 +9,72 @@
 
 VALUE rb_cGlimpl;
 
+static void *gl_load_library(const char *name)
+{
+  void *dl = NULL;
+
+#if defined(HAVE_DLFCN_H)
+  dl = dlopen(name, RTLD_LAZY | RTLD_LOCAL
+#if defined(RTLD_FIRST)
+    | RTLD_FIRST
+#endif
+  );
+  if (dl == NULL)
+    rb_raise(rb_eArgError,"Can't load OpenGL library %s: %s", name, dlerror());
+
+#elif defined(HAVE_WINDOWS_H)
+  dl = LoadLibrary(name);
+  if (dl == NULL) {
+    char error_text[999] = "LoadLibrary() error";
+    FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_MAX_WIDTH_MASK,
+                NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                (LPTSTR)&error_text, sizeof(error_text), NULL);
+    rb_raise(rb_eArgError,"Can't load OpenGL library %s: %s", name, error_text);
+  }
+#else
+#error No loading mechanism available
+#endif
+
+  return dl;
+}
+
+static void *gl_load_symbol(void *dl, const char *name, int raise)
+{
+  void *fptr = NULL;
+
+#if defined(HAVE_DLFCN_H)
+  fptr = dlsym(dl, name);
+  if (fptr == NULL && raise)
+    rb_raise(rb_eNotImpError,"Function %s is not available on this system: %s",name, dlerror());
+
+#elif defined(HAVE_WINDOWS_H)
+  fptr = GetProcAddress(dl, name);
+  if (fptr == NULL && raise){
+    char error_text[999] = "GetProcAddress() error";
+    FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_MAX_WIDTH_MASK,
+                NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                (LPTSTR)&error_text, sizeof(error_text), NULL);
+    rb_raise(rb_eNotImpError,"Function %s is not available on this system: %s",name, error_text);
+  }
+#else
+#error No loading mechanism available
+#endif
+
+  return fptr;
+}
+
+static void gl_close_library(void *dl)
+{
+#if defined(HAVE_DLFCN_H)
+  dlclose(dl);
+#elif defined(HAVE_WINDOWS_H)
+  FreeLibrary(dl);
+#else
+#error No loading mechanism available
+#endif
+}
+
+
 static struct glimpl *
 rb_glimpl_struct( VALUE self )
 {
@@ -24,7 +90,7 @@ static void *load_gl_function(VALUE self, const char *name, int raise)
     func_ptr = this->fptr_GetProcAddress(name);
 
   } else {
-    func_ptr = dlsym(this->dl, name);
+    func_ptr = gl_load_symbol(this->dl, name, 0);
 
     if(func_ptr == NULL)
     {
@@ -32,7 +98,7 @@ static void *load_gl_function(VALUE self, const char *name, int raise)
             char *symbol_name = ALLOC_N(char, strlen(name) + 2);
             symbol_name[0] = '_';
             strcpy(symbol_name + 1, name);
-            func_ptr = dlsym(this->dl, symbol_name);
+            func_ptr = gl_load_symbol(this->dl, symbol_name, 0);
             xfree(symbol_name);
     }
   }
@@ -67,7 +133,7 @@ rb_glimpl_mark( struct glimpl *this )
 
 static void rb_glimpl_free( struct glimpl *this )
 {
-  if(this->dl) dlclose(this->dl);
+  if(this->dl) gl_close_library(this->dl);
   this->dl = NULL;
   xfree(this);
 }
@@ -90,22 +156,13 @@ rb_glimpl_s_open(int argc, VALUE *argv, VALUE klass)
   this->error_checking = Qtrue;
   this->inside_begin_end = Qfalse;
   this->load_gl_function = load_gl_function;
-
-  this->dl = dlopen(p_dl_name, RTLD_LAZY | RTLD_LOCAL
-#if defined(RTLD_FIRST)
-    | RTLD_FIRST
-#endif
-  );
-  if (this->dl == NULL)
-    rb_raise(rb_eArgError,"Can't load OpenGL library %s", p_dl_name);
+  this->dl = gl_load_library(p_dl_name);
 
   if( NIL_P(proc_address_function) ){
     this->fptr_GetProcAddress = NULL;
   } else {
     char *p_proc_address_function = StringValueCStr(proc_address_function);
-    this->fptr_GetProcAddress = dlsym(this->dl, p_proc_address_function);
-    if (this->fptr_GetProcAddress == NULL)
-      rb_raise(rb_eNotImpError,"Function %s is not available on this system",p_proc_address_function);
+    this->fptr_GetProcAddress = gl_load_symbol(this->dl, p_proc_address_function, 1);
   }
 
   return self;
@@ -114,7 +171,7 @@ rb_glimpl_s_open(int argc, VALUE *argv, VALUE klass)
 static VALUE rb_glimpl_close(VALUE self)
 {
   struct glimpl *this = rb_glimpl_struct(self);
-  if(this->dl) dlclose(this->dl);
+  if(this->dl) gl_close_library(this->dl);
   this->dl = NULL;
   return Qnil;
 }
